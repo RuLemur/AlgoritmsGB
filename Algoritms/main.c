8dfc1440c15031fa8b1d9432259daa39a01ad4f0
Aleksandr Pavlov <aleksandpavlov@ozon.ru>
2020-12-27 14:43:46 +0300

    q

--- Algoritms/main.c
+++ Algoritms/main.c
@@ -6,9 +6,16 @@
 //
 
 #include <stdio.h>
-#include "lessonOne.h"
+//#include "lessonOne.h"
+#include "les"
 
 int main(){
+    //task 1
+    char result[255];
+    toTwo(12123, &result, 0);
+
+    
+/* lesson 1
 //    bodyIndex(80, 1.78);
 //    maxInFourDigits(6, 12, 1242, 112);
 //    digitSwap(32,45,0);
@@ -22,9 +29,8 @@ int main(){
 //    arithmeticAverage();
 //    maxInThreeDigits(1, 23, 4);
 //    randomer(13, 0);
-    avtomormfDigits(9376);
+//    avtomormfDigits(9376);
     
-    
-    
+*/
     return 0;
 }
